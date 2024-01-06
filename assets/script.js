c1a638a17b2ebd116f75e63b9b2d75d0bc95ebde
K-Manasaa <155672676+K-Manasaa@users.noreply.github.com>
2024-01-06 10:07:56 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// script.js
+
+// Smooth scrolling for anchor links
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollLinks = document.querySelectorAll('a[href^="#"]');
+    
+    for (const scrollLink of scrollLinks) {
+        scrollLink.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    }
+});
